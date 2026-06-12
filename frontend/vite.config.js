fc09072158fbe7f35629d141629320bf488d1e01
thefearlesscoder <thefunnelvkd@gmail.com>
2026-06-12 22:47:37 +0530

    build: include production assets and update configuration files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'build',
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
